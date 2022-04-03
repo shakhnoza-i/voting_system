8d8da2eaaf0e096dd315dee06942b765810b71ab
shaha <shaha0111@mail.ru>
2022-04-03 17:00:01 +0600

    db connection unstable

---
+++ config.py
@@ -0,0 +1,18 @@
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+from pydantic import BaseSettings, Field
+
+env_path = Path('.') / '.env'
+load_dotenv(dotenv_path=env_path)
+
+
+class Settings(BaseSettings):
+    POSTGRES_USER : str = os.getenv("POSTGRES_USER")
+    POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+    POSTGRES_SERVER : str = os.getenv("POSTGRES_SERVER","localhost")
+    POSTGRES_PORT : str = os.getenv("POSTGRES_PORT",5433) # default postgres port is 5432
+    POSTGRES_DB : str = os.getenv("POSTGRES_DB","my_db")
+    DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}"
+
+settings = Settings()
